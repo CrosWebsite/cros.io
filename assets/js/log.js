d9810139182dfc4ed196f22ca07bd027aa2f5a22
CrosWebsite <emirhan.zilifli31@gmail.com>
2021-02-28 13:02:26 +0300

    cros.io

--- assets/js/log.js
+++ assets/js/log.js
@@ -1,28 +1,21 @@
-log = {
-    // 0 - Only info
-    // 1 - Info & warns
-    // 2 - Info, warns & errors
-    // 3 - All
-    VERBOSITY: 3,
+// Developed by the CigarProject //
 
-    info: function(str) {
-		console.info("[INFO]", str);
+var log = {
+
+	info: function(str) {
+		console.debug("[INFO]", str);
 	},
 
 	warn: function(str) {
-        if (log.VERBOSITY < 1) return;
 		console.warn("[WARN]", str);
 	},
 
 	err: function(str) {
-        if (log.VERBOSITY < 2) return;
-		console.error("[ERROR]", str);
+		console.error("[ERROR] ", str);
 	},
 
 	debug: function(str) {
-        if (log.VERBOSITY < 3) return;
-		console.debug("[DEBUG]", str);
+		console.info("[DEBUG] ", str);
 	}
-}
 
-var LOAD_START = new Date();
+}
